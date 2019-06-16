839b3f26853ee8665ea56edc7d3529daa936b7c4
Luca Biordi <luca.biordi@codd-date.it>
2019-06-16 09:05:15 +0200

    Updates

---
+++ precache-manifest.517322cba3615e6ef7e2e592f6f0e94f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8075e39df57763ba4406fafdb3fc48ca",
+    "url": "/lucabio/demoReactApp.git/index.html"
+  },
+  {
+    "revision": "cbe276d401810c9b510a",
+    "url": "/lucabio/demoReactApp.git/static/css/main.eced0934.chunk.css"
+  },
+  {
+    "revision": "9bb52ebc66cae5eee18e",
+    "url": "/lucabio/demoReactApp.git/static/js/2.bdafdfdf.chunk.js"
+  },
+  {
+    "revision": "cbe276d401810c9b510a",
+    "url": "/lucabio/demoReactApp.git/static/js/main.b978f1f1.chunk.js"
+  },
+  {
+    "revision": "07726d8d68e9a0045ba2",
+    "url": "/lucabio/demoReactApp.git/static/js/runtime~main.8cff38e0.js"
+  }
+]);
